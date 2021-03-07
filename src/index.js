38e1a15462a927ce5bcfa13f78003802d4c21d9c
Jessica Justice <jessbjust@gmail.com>
2021-03-07 16:31:12 -0700

    Display user dashboard info MVP

--- src/index.js
+++ src/index.js
@@ -17,15 +17,41 @@ import Customer from './Customer';
 import Room from './Room';
 import Booking from './Booking';
 
+let allCustomers;
+let allBookings;
+let allRooms;
+
+let userGreeting = document.querySelector('.user-greeting');
+let totalSpent = document.querySelector('.total-spent');
+let bookingsList = document.querySelector('.bookings');
+
+
+
+
+
 function fetchData() {
   Promise.all([allCustomerDataAPI, bookingDataAPI, roomDataAPI])
     .then((values) => {
-      renderData(values[0], values[1], values[2])
+      createInstances(values);
+      renderUserDashboard()
     })
 }
 
-function renderData(customersData, bookingsData, roomsData) {
-  console.log(customersData.customers[12], bookingsData, roomsData)
+function createInstances(data) {
+  allCustomers = data[0].customers.map(customer => new Customer(customer));
+  allBookings = data[1].bookings.map(booking => new Booking(booking));
+  allRooms = data[2].rooms.map(room => new Room(room));
+}
+
+function renderUserDashboard() {
+  console.log(allCustomers[3].findMyBookings(allBookings));
+  userGreeting.innerText = allCustomers[3].name;
+  totalSpent.innerText = allCustomers[3].findTotalSpent(allRooms, allBookings).toFixed(2);
+  allCustomers[3].findMyBookings(allBookings).forEach(booking => {
+    bookingsList.innerHTML += `
+      <section class="item">Date: ${booking.date} Room Number: ${booking.roomNumber}</section>
+    `;
+  })
 }
 
 
