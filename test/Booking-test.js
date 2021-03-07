0a83fb055011308140a10bae1a0010fb3925c025
Jessica Justice <jessbjust@gmail.com>
2021-03-07 11:52:29 -0700

    Add tests for properties

--- test/Booking-test.js
+++ test/Booking-test.js
@@ -25,4 +25,24 @@ describe('Booking', function() {
     expect(bookingOne).to.be.an.instanceOf(Booking);
   });
 
+  it('should have an ID number', function() {
+    expect(bookingTwo.id).to.equal('5fwrgu4i7k55hl6uf');
+  });
+
+  it('should have a use ID associated with it', function() {
+    expect(bookingThree.userID).to.equal(3);
+  });
+
+  it('should have a date associated with it', function() {
+    expect(bookingOne.date).to.equal('2020/02/05');
+  });
+
+  it('should have a roomNumber associated with it',  function() {
+    expect(bookingTwo.roomNumber).to.equal(18);
+  });
+
+  it('should have room service charges listed', function() {
+    expect(bookingThree.roomServiceCharges).to.equal([]);
+  });
+
 });
