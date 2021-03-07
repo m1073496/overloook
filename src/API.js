748c429b0d4cc8a03817edd471b021c55a36dbe6
Jessica Justice <jessbjust@gmail.com>
2021-03-07 15:47:05 -0700

    Hook up APIs to index.js

---
+++ src/API.js
@@ -0,0 +1,19 @@
+const checkForErrors = response => {
+  if (!response.ok) {
+    throw new Error('Oops, something went wrong. Try again in a few! 😁');
+  } else {
+    return response.json();
+  }
+}
+
+export const allCustomerDataAPI = fetch("http://localhost:3001/api/v1/customers")
+  .then(checkForErrors)
+  .catch(err => alert(err))
+
+export const bookingDataAPI = fetch("http://localhost:3001/api/v1/bookings")
+  .then(checkForErrors)
+  .catch(err => alert(err))
+
+export const roomDataAPI = fetch("http://localhost:3001/api/v1/rooms")
+  .then(checkForErrors)
+  .catch(err => alert(err))
--- src/index.js
+++ src/index.js
@@ -7,4 +7,45 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+import {
+  allCustomerDataAPI,
+  bookingDataAPI,
+  roomDataAPI
+} from './API.js';
+
+import Customer from './Customer';
+import Room from './Room';
+import Booking from './Booking';
+
+function fetchData() {
+  Promise.all([allCustomerDataAPI, bookingDataAPI, roomDataAPI])
+    .then((values) => {
+      renderData(values[0], values[1], values[2])
+    })
+}
+
+function renderData(customersData, bookingsData, roomsData) {
+  console.log(customersData.customers[12], bookingsData, roomsData)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener('load', fetchData);
