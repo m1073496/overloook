ebeec0a4887d9ff4d22b5bb6731fd192ed81783f
Jessica Justice <jessbjust@gmail.com>
2021-03-07 11:44:34 -0700

    Build test skeleton

---
+++ test/Booking-test.js
@@ -0,0 +1,28 @@
+import {
+  expect
+} from 'chai';
+
+import Booking from '../src/Booking.js';
+import {
+  bookings
+} from '../src/data/test-data.js';
+
+describe('Room', function() {
+
+  let bookingOne, bookingTwo, bookingThree;
+
+  beforeEach(function() {
+    bookingOne = new Booking(bookings[0]);
+    bookingTwo = new Booking(bookings[1]);
+    bookingThree = new Booking(bookings[2]);
+  });
+
+  it('should be a function', function() {
+    expect(Booking).to.be.a('function');
+  });
+
+  it('should be an instance of a Room', function() {
+    expect(bookingOne).to.be.an.instanceOf(Booking);
+  });
+
+});
